801540302b03ace9c621be9fe808aebeb3c788e5
krishna-ammu05 <krishnaveni5104@gmail.com>
2025-09-14 19:07:54 +0530

    344. Reverse string(sol)

---
+++ RevStrInplace/Solution.java
@@ -0,0 +1,19 @@
+package RevStrInplace;
+
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            // swap characters
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            // move pointers
+            left++;
+            right--;
+        }
+    }
+}
